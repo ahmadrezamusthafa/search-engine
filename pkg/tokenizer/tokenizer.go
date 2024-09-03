278689bcb8ae25233c48bc05fb239099dc76db5d
reza <ahmad.reza.musthafa2@gmail.com>
2024-09-03 16:45:46 +0700

    fix tokenizer

--- pkg/tokenizer/tokenizer.go
+++ pkg/tokenizer/tokenizer.go
@@ -1,28 +1,37 @@
 package tokenizer
 
 import (
+	"bytes"
 	"github.com/ahmadrezamusthafa/search-engine/common/util"
 	"github.com/ahmadrezamusthafa/search-engine/internal/structs"
 	"strings"
 )
 
 func Tokenize(content structs.Content) []string {
-	strContent := strings.ToLower(content.String)
-	tokens := strings.Fields(strContent)
+	buf := bytes.Buffer{}
+	buf.WriteString(strings.ToLower(content.String))
 
 	if len(content.Object) > 0 {
 		if len(content.ObjectIndexes) > 0 {
 			for _, index := range content.ObjectIndexes {
 				if v, ok := content.Object[index]; ok {
-					tokens = append(tokens, util.InterfaceToString(v))
+					if buf.Len() > 0 {
+						buf.WriteString(" ")
+					}
+					buf.WriteString(util.InterfaceToString(v))
 				}
 			}
 		} else {
 			for _, v := range content.Object {
-				tokens = append(tokens, util.InterfaceToString(v))
+				if buf.Len() > 0 {
+					buf.WriteString(" ")
+				}
+				buf.WriteString(util.InterfaceToString(v))
 			}
 		}
 	}
+
+	tokens := strings.Fields(buf.String())
 
 	return tokens
 }
